f4d635322b81631caa8dd0f873d5250fdfa08041
Vinh Le <lethanhvinh95@gmail.com>
2021-02-18 18:11:56 +0200

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Vinh Le on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
