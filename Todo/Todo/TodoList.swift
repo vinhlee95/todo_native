64c41b09a92f6c31e6fb16b3ad8c86ebeaf04d4f
Vinh Le <lethanhvinh95@gmail.com>
2021-02-24 17:57:28 +0200

    semi-do editing todo feature

--- Todo/Todo/TodoList.swift
+++ Todo/Todo/TodoList.swift
@@ -16,6 +16,8 @@ class TodoViewModel: ObservableObject {
         .init(id: NSUUID().uuidString, title: "Learn investment", done: false),
         .init(id: NSUUID().uuidString, title: "Buy groceries", done: true),
     ]
+    
+    @Published var editingTitle = ""
 }
 
 struct TodoList: View {
@@ -25,14 +27,29 @@ struct TodoList: View {
     @ObservedObject var vm = TodoViewModel()
     @State var showCompleted = false
     
+    @State var editingId = ""
+    @State var isResponderEditing = true
+
     var body: some View {
         VStack {
             ScrollView {
                 ForEach(getRenderedTodos(todos: vm.todos), id: \.self) {todo in
-                    HStack {
-                        TodoItem(todo: todo, toggleComplete: toggleComplete)
-                        Spacer()
-                    }.padding(.leading)
+                    if todo.id == editingId {
+                        HStack {
+                            Image(systemName: "circle")
+                                .font(.system(size: 24, weight: .light))
+                                .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                            VStack(alignment: .leading) {
+                                CustomTextField(text: $vm.editingTitle, nextResponder: .constant(nil), isResponder: $isResponderEditing, onEditingEnd: onEditingSubmit, isSecured: false, keyboard: .default).padding(.bottom, 2)
+                                Divider()
+                            }
+                        }.padding(.bottom, 8).padding(.leading)
+                    } else {
+                        HStack {
+                            TodoItem(todo: todo, toggleComplete: toggleComplete, handleEditTodo: handleEditTodo)
+                            Spacer()
+                        }.padding(.leading)
+                    }
                 }
                 if showNewTodoField {
                     HStack {
@@ -79,6 +96,15 @@ struct TodoList: View {
         self.vm.todos.append(.init(id: NSUUID().uuidString, title: newTodoTitle, done: false))
     }
     
+    private func onEditingSubmit() {
+        self.vm.todos = self.vm.todos.map({ (todo) -> Todo in
+            if todo.id != editingId {
+                return todo
+            }
+            return Todo(id: todo.id, title: vm.editingTitle, done: todo.done)
+        })
+    }
+    
     private func toggleComplete(id: String) {
         self.vm.todos = self.vm.todos.map({ (todo) -> Todo in
             let toggledDone = todo.id == id ? !todo.done : todo.done
@@ -97,11 +123,20 @@ struct TodoList: View {
         
         return showCompleted ? inCompleteTodos + completedTodos : inCompleteTodos
     }
+    
+    private func handleEditTodo(id: String) {
+        editingId = id
+        let index = vm.todos.firstIndex { (todo) -> Bool in
+            return todo.id == id
+        }
+        vm.editingTitle = vm.todos[index!].title
+    }
 }
 
 struct TodoItem: View {
     let todo: Todo
     let toggleComplete: (_ id: String) -> Void
+    let handleEditTodo: (_ id: String) -> Void
     
     var body: some View {
         HStack {
@@ -110,10 +145,12 @@ struct TodoItem: View {
                     .font(.system(size: 24, weight: .light))
                     .foregroundColor(todo.done ? Color.red : Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
             })
-            VStack(alignment: .leading) {
-                Text(todo.title).padding(.top, 12)
-                Divider()
-            }
+            Button(action: {handleEditTodo(todo.id)}, label: {
+                VStack(alignment: .leading) {
+                    Text(todo.title).padding(.top, 12)
+                    Divider()
+                }.foregroundColor(.black)
+            })
         }.padding(.bottom, 8)
     }
 }
